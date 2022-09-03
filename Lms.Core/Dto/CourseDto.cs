b31a3bd7fe32b9722efd491324ad59e78815c5f7
Lexicon Student 1 <Lexicon Student 1@DESKTOP-OF8V11H>
2022-09-03 14:40:41 +0200

    Added CourseDto and ModuleDto

---
+++ Lms.Core/Dto/CourseDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Lms.Core.Dto
+{
+    public class CourseDto
+    {
+        public string Title { get; set; } = "";
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; private set; }
+
+        public CourseDto(string title, DateTime startDate) {
+            Title = title;
+            StartDate = startDate;
+            EndDate = startDate.AddMonths(3);
+        }
+    }
+}
---
+++ Lms.Core/Dto/ModuleDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Lms.Core.Dto
+{
+    public class ModuleDto
+    {
+        public string Title { get; set; } = "";
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; private set; }
+
+        public ModuleDto(string title, DateTime startDate) {
+            Title = title;
+            StartDate = startDate;
+            EndDate = startDate.AddMonths(1);
+        }
+
+    }
+}
